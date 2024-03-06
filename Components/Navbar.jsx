fab185438db06246458156c7441582e80620f3c5
Kaushik <kaushikshetty6979@gmail.com>
2024-03-06 15:47:05 +0530

    changes

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,24 +1,23 @@
 import { ConnectWallet, useAddress } from '@thirdweb-dev/react'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Navbar() {
     const address = useAddress();
 
     return (
-        <div >
-            <div className={styles.navbar}>
+        <div>
+            <div className="bg-gray-900 text-white px-4 py-2 flex flex-wrap justify-between items-center ">
                 <Link href="/">
-                    <p>NFT Collection</p>
+                    <p className="cursor-pointer mb-4 sm:mb-0">NFT Collection</p>
                 </Link>
-                <div className={styles.navLinks}>
+                <div className="flex items-center mb-4 sm:mb-0">
                     {address && (
                         <Link href={`profile/${address}`}>
-                            <p>My NFTs</p>
+                            <p className="cursor-pointer mr-4">My NFTs</p>
                         </Link>
                     )}
                 </div>
-                <ConnectWallet />
+                <ConnectWallet className="bg-gradient-to-r from-purple-500 to-pink rounded-md px-6 py-2 cursor-pointer text-white text-base" />
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,47 @@
-import styles from "../styles/Home.module.css";
-import { MediaRenderer, Web3Button, useActiveClaimConditionForWallet, useAddress, useClaimIneligibilityReasons, useContract, useContractMetadata, useTotalCirculatingSupply, useTotalCount } from "@thirdweb-dev/react";
-// import { CONTRACT_ADDRESS } from "../const/addresses";
-// import { ethers } from "ethers";
-import { useRouter } from "next/router";
+import { MediaRenderer, Web3Button } from "@thirdweb-dev/react";
 import { useState } from "react";
 
 const Home = () => {
-  // const address = useAddress();
-  // const router = useRouter();
-  // const maxClaimQuantity = 2;
+  const [claimQuantity, setClaimQuantity] = useState(1);
+  const maxClaimQuantity = 2;
 
-  // const { contract } = useContract(CONTRACT_ADDRESS);
-
-  // const { data: contractMetadata, isLoading: isContractMetadataLoading } = useContractMetadata(contract);
-
-  // const { data: activeClaimPhase, isLoading: isActiveClaimPhaseLoading } = useActiveClaimConditionForWallet(contract, address);
-
-  // const { data: claimIneligibilityReasons, isLoading: isClaimIneligibilityReasonsLoading } = useClaimIneligibilityReasons(contract, {
-  //   walletAddress: address || "",
-  //   quantity: 1
-  // });
-
-  // const { data: totalSupply, isLoading: isTotalSupplyLoading } = useTotalCount(contract);
-  // const { data: totalClaimSupply, isLoading: isTotalClaimSupplyLoading } = useTotalCirculatingSupply(contract);
-
-  // const [claimQuantity, setClaimQuantity] = useState(1);
   const increment = () => {
     if (claimQuantity < maxClaimQuantity) {
       setClaimQuantity(claimQuantity + 1);
     }
   };
+
   const decrement = () => {
     if (claimQuantity > 1) {
       setClaimQuantity(claimQuantity - 1);
     }
   };
 
-  return /*#__PURE__*/(
-    <div className={styles.container}>
-      <main className={styles.main}>
-      
-          <div className={styles.heroSection}>
-            <div className={styles.collectionImage}>
-              <MediaRenderer  />
-            </div>
-            <div>
-              <h1>Name</h1>
-              <p>desc</p>
-
-              <div>
-                <p>Eligible to claim</p>
-                <div className={styles.claimContainer}>
-                  <div className={styles.claimValue}>
-                    <button className={styles.claimBtn} onClick={decrement}>-</button>
-                    <input className={styles.claimInput} type="number"  />
-                    <button className={styles.claimBtn} onClick={increment}>+</button>
-                  </div>
-                  <Web3Button >
-                    Claim NFT
-                  </Web3Button>
-                 
-                </div>
+  return (
+    <div className="bg-black w-full h-full">
+      <div className="flex flex-col md:flex-row justify-between items-center mx-auto mt-3 md:w-3/4 lg:w-1/2 bg-gray-900 text-white px-4 py-2 rounded-md mb-8">
+        <div className="collectionImage mr-0 md:mr-5 p-14">
+          <MediaRenderer />
+        </div>
+        <div className="p-8">
+          <h1 className="text-3xl font-bold">Name</h1>
+          <p className="text-lg">Description</p>
+    
+          <div className="mt-4">
+            <p className="text-lg">Eligible to claim</p>
+            <div className="flex items-center">
+              <div className="flex items-center">
+                <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded-md mr-2" onClick={decrement}>-</button>
+                <input className="bg-gray-800 text-white px-4 py-2 rounded-md w-16 text-center" type="number" value={claimQuantity} readOnly />
+                <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded-md ml-2" onClick={increment}>+</button>
               </div>
+              <button className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-black px-4 py-2 rounded-md mt-4 md:mt-0">
+                Claim NFT
+              </button>
             </div>
           </div>
-  
-      </main>
+        </div>
+      </div>
     </div>
   );
 };
