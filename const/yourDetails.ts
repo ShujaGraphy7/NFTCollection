69f009fc22c94194fa3c8e90fb34136568146b61
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-03-06 23:24:16 +0500

    update again

---
+++ const/yourDetails.ts
@@ -0,0 +1,5 @@
+// Replace this with your contract address
+export const myEditionDropContractAddress: string = "0xEF1A6f5B596d0178f27A5c81Be803f8Ed5fcC3F4";
+
+// Replace this with your token id
+export const tokenId: string = "0";
