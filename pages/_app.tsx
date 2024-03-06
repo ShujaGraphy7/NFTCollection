832bb425580428e08b8d996e4218370b4f91abe8
ShujaGraphy7 <shujaabrar7@gmail.com>
2024-03-06 23:25:55 +0500

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChainId}>
       <Head>
-        <title>thirdweb Edition Drop Minting Customizable Page</title>
+        <title>OG NFT Collection</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
