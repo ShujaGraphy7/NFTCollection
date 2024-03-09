0e1889be420a5a325a2b03277ada27961d8ef9eb
Kaushik <kaushikshetty6979@gmail.com>
2024-03-09 17:53:38 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Theme.module.css'
+import github from '../logo/github.png'
+import telegram from '../logo/telegram.png'
+import twitter from '../logo/twitter.png'
+import Image from 'next/image';
 
-const Footer = () => {
+const Footer= () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.socialIcons}>
+        <a href="https://github.com"  className={styles.socialImg}>
+          <Image src={github} alt="GitHub Logo" />
+        </a>
+        <a href="https://telegram.org"  className={styles.socialImg}>
+          <Image src={telegram} alt="Telegram Logo" />
+        </a>
+        <a href="https://twitter.com"  className={styles.socialImg}>
+          <Image src={twitter} alt="Twitter Logo" />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
